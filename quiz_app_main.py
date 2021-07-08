3bd4e905da19e6f298bb3b39315f2618c6199486
Perembowei Suoye <suoyepere@ymail.com>
2021-07-08 19:20:41 +0100

    chore: refactored the code

--- quiz_app_main.py
+++ quiz_app_main.py
@@ -2,7 +2,15 @@ import json
 import random
 
 with open("quiz_app_question.json") as f:
-    questions = json.load(f)
+   questions = json.load(f)
 
 def play_quiz():
     
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+questions = [
+    Question(question_)
+]

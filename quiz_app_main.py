476eadb267668342fb2828a42e8e4e8dc0ba6cb4
Perembowei Suoye <suoyepere@ymail.com>
2021-07-25 20:14:39 +0100

    updated the app

--- quiz_app_main.py
+++ quiz_app_main.py
@@ -2,27 +2,14 @@ import json
 import random
 
 
-questions_prompts = ["What year did Nigeria get independence?\n(a)1959\n(b)1960\n(c)1961\n(d)1962", 
-"How many primary colours are there?\n(a)1\n(b)2\n(c)3\n(d)4", 
-"What colour are Strawberries?\n(a)yellow\n(b)red\n(c)blue\n(d)green",]
-
-file_name = "quiz_app_question.json"
-
-with open(file_name, "w") as f:
-   json.dump(questions_prompts, f)
-
-with open(file_name) as f:
-    questions = json.load(f)
+with open("questions.json") as f:
+   questions = json.load(f)
 
 class Question:
     def __init__(self, prompt, answer):
         self.prompt = prompt
         self.answer = answer
 
-questions_and_answers = [
-    Question(questions_prompts[0], "b"),
-    Question(questions_prompts[1], "c"),
-    Question(questions_prompts[2], "b"),
 ]
 def play_quiz(questions_prompts):
     score = 0
@@ -32,4 +19,5 @@ def play_quiz(questions_prompts):
             score += 1
     print(f"You got {str(score)} / {str(len(questions))} correct.")
 
-play_quiz(questions)
+print(questions)
+
